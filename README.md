be86331096667350162987b58b5364e1e3a5fd91
Laurent Ghigonis <ooookiwi@gmail.com>
2021-04-06 02:03:25 +0200

    complete rewrite

--- README.md
+++ README.md
@@ -1,64 +1,72 @@
 cqemu
 =====
 
-qemu VM command-line manager
+simple qemu VM command-line manager
 
 ### Usage
 
 ```bash
-usage: cqemu [-ph] (new|start|show|rm|ls) [vm-name] [options]
-    -p : pretend, only print commands and do not execute anything
-    -h : this help
+$ cqemu
+usage: cqemu [-ph] (new|start|show|spice|ssh vm-dir [options]) | show-profiles
+   -p : pretend, only print commands and do not execute anything
+   -h : this help
 
-Actions:
-    new <vm-name> (template <vm-name> | <disk-size> <conf-profile>) [-o conf-options] [qemu-options...]
-    start <vm-name> [-o conf-options] [qemu-options...]
-    show <vm-name>
-    rm <vm-name>
-    ls
-
-Default conf-options, per conf-profile:
-linux-desk : cores=2;ram="2G";virtio_disk=1;internet=0;share="";display="qxl"
-linux-server : cores=2;ram="2G";virtio_disk=0;internet=1;share="";display="curses"
-windows : cores=2;ram="2G";virtio_disk=0;internet=0;share="/home/share";display="qxl"
-
-Examples:
-    cqemu new myvm 20GB linux-desk
-    cqemu new myvm 20GB windows
-    cqemu new myvm 20GB linux-server
-    cqemu new myvm template basevm
-    cqemu new myvm template basevm internet=1
-    cqemu start myvm
-    cqemu start myvm -o share=/mnt/usb
-    cqemu start myvm -cdrom /data/mycd.iso
+actions
+   new <vm-dir> <profile_name> <disk_size> <network_mode>
+   start <vm-dir> [<network_mode>] [<display_mode>] [qemu-options...]
+   show <vm-dir>
+   spice <vm_name>
+   ssh <vm_name>
+   show-profiles
+profiles
+   linux_desk linux_serv raspi3 windows
+network_mode
+   net-none net-user net-tap[-<ip>/<mask>]
+display_mode
+   display-none display-curses display-sdl display-qxl
+environnment variables
+   QEMU_RUNAS=nobody
+   SPICE_CLIENT=spicy
+examples
+   cqemu new mylinux linux-desk 20G net-user
+   cqemu new mywindows windows 20G net-none
+   cqemu new myserver linux-server 20G net-tap-192.168.0.1/24
+   cqemu start mylinux
+   cqemu start mylinux net-none -cdrom /data/mycd.iso
 ```
 
-### Dependencies
-
-* spicy (spice-gtk package) : spice client used by default, can be changed in ~/.cqemu/cqemurc
-* qemu of course
-* KVM enabled kernel
-
-### Examples
-
-#### Create a Windows 7 VM
-
-```base
-$ cqemu new win7 20GB windows
-
-[-] Running mkdir /home/user/vms/win7
-[-] Running qemu-img create -f raw -o size=20GB /home/user/vms/win7/disk.img
-Formatting '/home/user/vms/win7/disk.img', fmt=raw size=21474836480
-[-] Running ln -s /home/user/.cqemu/conf.profile.windows > /home/user/vms/win7/conf_profile
-[*] Created VM /home/user/vms/win7
-```
-
-#### Start the Windown 7 VM with the install CD
+#### Profiles and modes
 
 ```bash
-$ cqemu start win7 -cdrom /home/user/win7install.iso
+$ cqemu show-profiles
+--- profiles ---
+linux_desk (display-qxl)
+   sudo qemu-system-x86_64 --enable-kvm -cpu host -smp cores=2 -m 2G -device intel-hda -device hda-duplex -drive file=/disk.img,if=virtio,format=raw -chroot /var/empty -runas nobody -sandbox on,obsolete=deny,spawn=deny
+linux_serv (display-sdl)
+   sudo qemu-system-x86_64 --enable-kvm -cpu host -smp cores=2 -m 2G -drive file=/disk.img,format=raw -chroot /var/empty -runas nobody -sandbox on,obsolete=deny,spawn=deny
+raspi3 (display-sdl)
+   sudo qemu-system-aarch64 -M raspi3 -kernel /kernel.img -chroot /var/empty -runas nobody -sandbox on,obsolete=deny,spawn=deny
+windows (display-qxl)
+   sudo qemu-system-x86_64 --enable-kvm -cpu host -smp cores=2 -m 4G -drive file=/disk.img -chroot /var/empty -runas nobody -sandbox on,obsolete=deny,spawn=deny
+--- network modes ---
+net-none : -nic none
+net-user : -netdev user,id=net0,hostfwd=tcp:127.0.0.1:-:22,hostfwd=tcp:127.0.0.1:-:21 -device virtio-net-pci,netdev=net0
+net-tap[-<ip>/<mask>] : -netdev tap,id=net0,ifname=,script=no,downscript=no -device virtio-net-pci,netdev=net0
+--- display modes ---
+display-none : -display none
+display-curses : -display curses
+display-sdl : 
+display-qxl : -vga none -device qxl-vga,max_outputs=1,vgamem_mb=256,vram_size_mb=256 -spice port=,disable-ticketing -device virtio-serial-pci -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent
+```
 
-spice_port=4290
-[-] Running $(sleep 2; spicy --title "win7_bin (spice_port=4290)" -h 127.0.0.1 -p 4290 </dev/null >/dev/null 2>/dev/null) &
-[-] Running qemu-system-x86_64 --enable-kvm -cpu host -smp cores=2 -m 2G -soundhw hda -drive file=/home/user/vms/win7/disk.img -net user,restrict=on,smb=/home/share -net nic,model=virtio -display curses -vga qxl -global qxl-vga.vram_size=64000000 -global qxl-vga.vram_size_mb=64000000 -global qxl-vga.vgamem_mb=32000000 -spice port=4290,disable-ticketing -device virtio-serial-pci -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent -cdrom /home/user/win7install.iso
+### Dependencies and requirements
+
+* qemu of course
+* if desktop profiles are used: KVM enabled kernel
+* if display qxl used: spicy (spice-gtk package), the spice client used by default
+
+### Installation
+
+```bash
+$ sudo make install
 ```
--- cqemu.sh
+++ cqemu.sh
@@ -1,165 +1,248 @@
-#!/bin/sh
+#!/bin/bash
 
-# cqemu - qemu VM command-line manager
-# 2015, Laurent Ghigonis <laurent@gouloum.fr>
-
-PROG=$(basename $0)
-CONF="$HOME/.cqemu"
-
-set -e
-
-# HELPERS
+# cqemu - simple qemu VM command-line manager
+# 2015 Laurent Ghigonis <ooookiwi@gmail.com> initial version
+# 2021 Laurent Ghigonis <ooookiwi@gmail.com> complete rewrite
 
 usageexit() {
 	cat <<-_EOF
-	usage: $PROG [-ph] (new|start|show|rm|ls) [vm-name] [options]
-	    -p : pretend, only print commands and do not execute anything
-	    -h : this help
+	usage: $PROG [-ph] (new|start|show|spice|ssh vm-dir [options]) | show-profiles
+	   -p : pretend, only print commands and do not execute anything
+	   -h : this help
 
-	Actions:
-	    new <vm-name> (template <vm-name> | <disk-size> <conf-profile>) [-o conf-options] [qemu-options...]
-	    start <vm-name> [-o conf-options] [qemu-options...]
-	    show <vm-name>
-	    rm <vm-name>
-	    ls
-
-	Default conf-options, per conf-profile:
-	$(find $CONF/conf.profile.* |while read c; do echo "$(echo $c |sed "s/.*\.\(.*\)/\1/g") : $(cat $c)"; done)
-
-	Examples:
-	    $PROG new myvm 20GB linux-desk
-	    $PROG new myvm 20GB windows
-	    $PROG new myvm 20GB linux-server
-	    $PROG new myvm template basevm
-	    $PROG new myvm template basevm internet=1
-	    $PROG start myvm
-	    $PROG start myvm -o share=/mnt/usb
-	    $PROG start myvm -cdrom /data/mycd.iso
+	actions
+	   new <vm-dir> <profile_name> <disk_size> <network_mode>
+	   start <vm-dir> [<network_mode>] [<display_mode>] [qemu-options...]
+	   show <vm-dir>
+	   spice <vm_name>
+	   ssh <vm_name>
+	   show-profiles
+	profiles
+	   $PROFILES
+	network_mode
+	   $NETWORK_MODES
+	display_mode
+	   $DISPLAY_MODES
+	environnment variables
+	   QEMU_RUNAS=$QEMU_RUNAS
+	   SPICE_CLIENT=$SPICE_CLIENT
+	examples
+	   $PROG new mylinux linux-desk 20G net-user
+	   $PROG new mywindows windows 20G net-none
+	   $PROG new myserver linux-server 20G net-tap-192.168.0.1/24
+	   $PROG start mylinux
+	   $PROG start mylinux net-none -cdrom /data/mycd.iso
 	_EOF
 	exit 1
 }
+PROFILES="linux_desk linux_serv raspi3 windows"
+NETWORK_MODES="net-none net-user net-tap[-<ip>/<mask>"]
+DISPLAY_MODES="display-none display-curses display-sdl display-qxl"
 
-conf_init() {
-	[ ! -d $CONF ] && echo "[*] First run, creating $CONF" && echo && mkdir -p $CONF
-	[ ! -e $CONF/conf.profile.windows ] && echo -e "cores=2;ram=\"2G\";virtio_disk=0;internet=0;share=\"/home/share\";display=\"qxl\"" > $CONF/conf.profile.windows
-	[ ! -e $CONF/conf.profile.linux-desk ]   && echo -e "cores=2;ram=\"2G\";virtio_disk=1;internet=0;share=\"\";display=\"qxl\"" > $CONF/conf.profile.linux-desk
-	[ ! -e $CONF/conf.profile.linux-server ]  && echo -e "cores=2;ram=\"2G\";virtio_disk=0;internet=1;share=\"\";display=\"curses\"" > $CONF/conf.profile.linux-server
-	[ ! -e $CONF/cqemurc ] && echo -e "vms=$HOME/vms\nspice_client=spicy" > $CONF/cqemurc
-	source $CONF/cqemurc
+err() { echo "$PROG error: $1" >&2; exit 1; }
+trace() { echo "# $*" >&2; [ $pretend -eq 1 ] && return; "$@" ||exit 10; }
+
+set_vm_vars() {
+	# sets vm_* variables from vm_name argument
+	name="$1"
+	vm_name="$(basename $1)"
+	vm_path="$(readlink -f $vm_name)"
+	vm_spice_port="9$(echo $vm_path |md5sum |tr -d 'a-z' |cut -c-3)"
+	vm_ssh_port_host="$(($vm_spice_port + 1))"
+	vm_ftp_port_host="$(($vm_spice_port + 2))"
 }
 
-err() { echo "$prog Error: $1"; exit $2; }
-trace() { echo "[-] Running $@"; [ $pretend -eq 1 ] && return; eval "$@" ||exit 10; }
-
-# ACTIONS
-
-do_new() {
-	[ $# -lt 2 ] && usageexit
-	trace mkdir $vmdir
-
-	if [ X"$1" = X"template" ]; then
-		trace cp -Rp $vms/$2/* $vmdir/
-	else
-		disksize="$1"
-		profile="$CONF/conf.profile.$2"
-		[ ! -e $profile ] && echo "ERROR: profile $2 does not exist !" && exit
-		trace qemu-img create -f raw -o size="$disksize" $vmdir/disk.img
-		trace "ln -s $profile $vmdir/conf_profile"
-	fi
-	shift; shift
-
-	if [ X"$1" = X"-o" ]; then
-		trace "echo $2 > $vmdir/conf"
-		shift; shift
-	fi
-
-	if [ X"$1" != X"" ]; then
-		trace echo "$@ > $vmdir/qemu_options"
-	fi
-
-	echo "[*] Created VM $vmdir"
+vm_conf_load() {
+	[ ! -d $vm_path ] && err "VM directory does not exist : $vm_path"
+	. "$vm_path/conf"
 }
 
-do_start() {
-	source $vmdir/conf_profile
-	[ -e $vmdir/conf ] && source $vmdir/conf
-
-	cmd="qemu-system-x86_64"
-	cmd="$cmd --enable-kvm "
-	cmd="$cmd -cpu host -smp cores=$cores"
-	cmd="$cmd -m $ram"
-	cmd="$cmd -soundhw hda "
-	[ $virtio_disk -eq 1 ] \
-		&& cmd="$cmd -drive file=$vmdir/disk.img,if=virtio " \
-		|| cmd="$cmd -drive file=$vmdir/disk.img "
-	if [ $internet -eq 0 ]; then
-		[ X"$share" != X"" ] \
-			&& cmd="$cmd -net user,restrict=on,smb=$share -net nic,model=virtio " \
-			|| cmd="$cmd -net none "
-	else
-		[ X"$share" != X"" ] \
-			&& cmd="$cmd -net user,smb=$share -net nic,model=virtio " \
-			|| cmd="$cmd -net none -net nic,model=virtio "
-	fi
-	if [ $display = "qxl" ]; then
-		spice_port=$(($RANDOM % 500 + 4000))
-		echo "spice_port=$spice_port"
-		cmd="$cmd -display curses "
-		cmd="$cmd -vga qxl -global qxl-vga.vram_size=64000000  -global qxl-vga.vram_size_mb=64000000 -global qxl-vga.vgamem_mb=32000000 "
-		cmd="$cmd -spice port=${spice_port},disable-ticketing "
-		cmd="$cmd -device virtio-serial-pci -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent "
-		trace "\$(sleep 2; $spice_client --title \"$vmname (spice_port=${spice_port})\" -h 127.0.0.1 -p ${spice_port} </dev/null >/dev/null 2>/dev/null) &"
-	fi
-	cmd="$cmd $opts "
-	cmd="$cmd $@"
-
-	trace $cmd
+spice_client_start() {
+	spice_cmd="$conf_pre $SPICE_CLIENT --title ${vm_name}...port=${vm_spice_port} -h 127.0.0.1 -p ${vm_spice_port} </dev/null >/dev/null 2>/dev/null) &"
+	echo "delaying spice client on port ${vm_spice_port} : $spice_cmd"
+	[ $pretend -eq 0 ] || return 0
+	$(sleep 2; $spice_cmd) &
 }
 
-do_show() {
-	find $vmdir |tail -n+2 |sort |while read f; \
-		do [ $(basename $f) = "disk.img" ] && continue; echo "=== $f ==="; cat $f; done
+set_profile_vars() {
+	profile="$1"
+	viewonly="$2"
+	LINUX_DEFAULTS="qemu-system-x86_64 --enable-kvm -cpu host -smp cores=2 -m 2G"
+	case $profile in
+	linux_desk)
+		cmd="sudo $LINUX_DEFAULTS -device intel-hda -device hda-duplex -drive file=$vm_path/disk.img,if=virtio,format=raw"
+		display="display-qxl"
+		;;
+	linux_serv)
+		cmd="sudo $LINUX_DEFAULTS -drive file=$vm_path/disk.img,format=raw"
+		display="display-sdl"
+		;;
+	raspi3)
+		kernel="$vm_path/kernel.img"
+		[ -z "$viewonly" -a ! -e $kernel ] && err "profile raspi3: kernel image '$kernel' does not exist"
+		cmd="sudo qemu-system-aarch64 -M raspi3 -kernel $kernel"
+		display="display-sdl"
+		;;
+	windows)
+		cmd="sudo qemu-system-x86_64 --enable-kvm -cpu host -smp cores=2 -m 4G -drive file=$vm_path/disk.img"
+		display="display-qxl"
+		;;
+	*)
+		err "invalid profile: $profile'. choices: $PROFILES"
+		;;
+	esac
+	cmd="$cmd -chroot /var/empty -runas $QEMU_RUNAS -sandbox on,obsolete=deny,spawn=deny"
+	profile_qemu_cmd="$cmd"
+	profile_display_mode="$display"
 }
 
-do_rm() {
-	trace ls $vmdir
-	echo "[?] Are you sure to \"rm -rf $vmdir\" ? [Enter / Ctrl-C]"; read ;
-	trace rm -rf $vmdir
+set_qemu_display() {
+	display=$1
+	viewonly=$2
+	case $display in
+		display-none) qemu_display="-display none" ;;
+		display-curses) qemu_display="-display curses" ;;
+		display-sdl) qemu_display="" ;;
+		display-qxl)
+			# max_outputs=1 : workaround QEMU 4.1.0 regression with QXL video
+			# see https://wiki.archlinux.org/index.php/QEMU#QXL_video_causes_low_resolution
+			qemu_display="-vga none -device qxl-vga,max_outputs=1,vgamem_mb=256,vram_size_mb=256 -spice port=${vm_spice_port},disable-ticketing -device virtio-serial-pci -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent"
+			[ ! -z "$viewonly" ] && return
+			spice_client_start
+			;;
+		*) err "invalid display mode: $display. choices: $DISPLAY_MODES" ;;
+	esac
 }
 
-do_ls() {
-	echo -e "$(du -Lhcs $vms |tail -n1 |cut -d\t -f1)\t$vms"
-	find -L $vms -maxdepth 1 -type d |tail -n+2 |sort |while read v; \
-		do echo -e "$(du -Lhcs $v |tail -n1 |cut -d\t -f1)\t$(basename $v)"; done
+set_qemu_net() {
+	net=$1
+	viewonly=$2
+	case $net in
+		net-none) qemu_net="-nic none" ;;
+		net-user) qemu_net="-netdev user,id=net0,hostfwd=tcp:127.0.0.1:${vm_ssh_port_host}-:22,hostfwd=tcp:127.0.0.1:${vm_ftp_port_host}-:21 -device virtio-net-pci,netdev=net0" ;;
+		net-tap*)
+			ip="$(echo $net |cut -d- -f3)"
+			qemu_net="-netdev tap,id=net0,ifname=$iface,script=no,downscript=no -device virtio-net-pci,netdev=net0"
+			[ ! -z "$viewonly" ] && return
+			iface="tap-$vm_name"
+			user=$(whoami)
+			$conf_pre ip a s dev $iface >/dev/null 2>&1 || \
+				trace $conf_pre sudo ip tuntap add user $user mode tap name $iface
+			[ ! -z "$ip" ] && trace $conf_pre sudo ip a a $ip dev $iface
+			trace $conf_pre sudo ip link set $iface up promisc on
+			trace sudo sysctl -w net.bridge.bridge-nf-call-iptables=0
+			trace sudo sysctl -w net.bridge.bridge-nf-call-ip6tables=0
+			trace sudo sysctl -w net.bridge.bridge-nf-call-arptables=0
+			;;
+		*) err "invalid network mode: $net. choices: $NETWORK_MODES" ;;
+	esac
 }
 
-# MAIN
+PROG=$(basename $0)
+QEMU_RUNAS=${QEMU_RUNAS:-nobody}
+SPICE_CLIENT="${SPICE_CLIENT:-spicy}"
 
-conf_init
-[ X"$1" = X"ls" ] && do_ls && exit 0
-[ $# -lt 2 -o X"$1" = X"-h" ] && usageexit
-pretend=0; [ X"$1" = X"-p" ] && pretend=1 && shift
-action=$1
-vmname=$2
-vmdir=$(readlink -f "$vms/$vmname")
-shift; shift
+set -e
 
+[ $# -lt 1 -o "$1" = "-h" ] && usageexit
+pretend=0
+[ "$1" = "-p" ] && echo "pretend (-p) active, no commands will be executed" && pretend=1 && shift
+
+action="$1"
+shift
 case $action in
 new)
-	[ $# -lt 2 ] && usageexit
-	do_new $@
+	[ $# -lt 4 ] && usageexit
+	name=$1
+	set_vm_vars $name
+	[ -d $vm_path ] && err "VM already exists in $vm_path"
+	profile=$2
+	disk_size=$3
+	net_mode=$4
+	echo "[+] validating VM profile"
+	set_profile_vars $profile
+	set_qemu_net $net_mode viewonly
+	echo "[+] creating new VM configuration"
+	mkdir "$vm_path"
+	cat > "$vm_path/conf" <<-_EOF
+# cqemu configuration file for VM '$vm_name'
+# creation date : $(date +%Y%m%d_%H%M%S)
+# creation host : $(uname -a)
+# creation path : $vm_path
+conf_qemu_cmd_base="$profile_qemu_cmd"
+conf_qemu_cmd_opts=""
+conf_display="$profile_display_mode"
+conf_net="$net_mode"
+conf_pre=""
+_EOF
+	trace cat "$vm_path/conf"
+	echo "[+] creating new VM image"
+	trace qemu-img create -f raw -o size="$disk_size" $vm_path/disk.img
+	echo "[*] DONE, new VM '$vm_name' created in $vm_path"
 	;;
 start)
-	do_start $@
+	[ $# -lt 1 ] && usageexit
+	name=$1
+	shift
+	set_vm_vars $name
+	vm_conf_load
+	while true; do case $1 in
+		net-*) conf_net=$1; shift ;;
+		display-*) conf_display=$1; shift ;;
+		-*) shift; qemu_user_opts="$@"; break ;;
+		"") break ;;
+		*) err "invalid option: $@" ;;
+	esac done
+	echo "pre command  : $conf_pre"
+	echo "network mode : $conf_net"
+	echo "display mode : $conf_display"
+	trace sudo date # get sudo password before qemu
+	set_qemu_net $conf_net
+	set_qemu_display $conf_display
+	trace $conf_pre $conf_qemu_cmd_base $qemu_display $qemu_net $conf_qemu_cmd_opts $qemu_user_opts
 	;;
 show)
-	do_show $@
+	[ $# -lt 1 ] && usageexit
+	name=$1
+	set_vm_vars $name
+	echo "configuration for VM '$vm_name':"
+	[ ! -e $vm_path/conf ] && err "VM configuration not found in $vm_path/conf"
+	trace cat $vm_path/conf
+	trace qemu-img info $vm_path/disk.img
 	;;
-rm)
-	do_rm $@
+spice)
+	[ $# -lt 1 ] && usageexit
+	name=$1
+	set_vm_vars $name
+	vm_conf_load
+	spice_client_start
+	;;
+ssh)
+	[ $# -lt 1 ] && usageexit
+	name=$1
+	set_vm_vars $name
+	vm_conf_load
+	trace $conf_pre ssh -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -p $vm_ssh_port_host u@127.0.0.1 "$@"
+	;;
+show-profiles)
+	echo "--- profiles ---"
+	for p in $PROFILES; do
+		set_profile_vars $p viewonly
+		echo "$p ($profile_display_mode)"
+		echo "   $profile_qemu_cmd"
+	done
+	echo "--- network modes ---"
+	for n in $NETWORK_MODES; do
+		set_qemu_net $n viewonly
+		echo "$n : $qemu_net"
+	done
+	echo "--- display modes ---"
+	for d in $DISPLAY_MODES; do
+		set_qemu_display $d viewonly
+		echo "$d : $qemu_display"
+	done
 	;;
 *)
 	usageexit
 	;;
 esac
-
